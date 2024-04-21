e4fe2050f74ae45d18a514f45a6c118c6533284c
4eam3less <4eam3less@gmail.com>
2024-04-21 15:50:49 +0200

    add quadratic equations and tests

---
+++ homework1/main.cpp
@@ -0,0 +1,6 @@
+#include "quadratic-equation.hpp"
+
+int main(){
+    solve(1,1,1);
+    return 0;
+}
---
+++ homework1/quadratic-equation.hpp
@@ -0,0 +1,4 @@
+#pragma once
+#include <array>
+
+ std::array<double, 2> solve(double a, double b, double c);
