9d8165ed032597e350f6bade7f68da5f29423cc4
4eam3less <4eam3less@gmail.com>
2024-05-08 17:13:19 +0200

    add const& to set_angle and set_angle

--- homework2/rotable.hpp
+++ homework2/rotable.hpp
@@ -5,6 +5,6 @@ struct Angle;
 class IRotable{
 public:
     virtual Angle get_angle() = 0;
-    virtual void set_angle(Angle) = 0;
+    virtual void set_angle(const Angle &angle) = 0;
     virtual Angle get_angular_velocity() = 0;
 };
