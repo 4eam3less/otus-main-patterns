16291826f16bd672ff8686e65ad423509d28f2bc
4eam3less <4eam3less@gmail.com>
2024-06-14 00:03:36 +0200

    fix warnings

--- commands/fuel-commands/fuel-manager-interface.hpp
+++ commands/fuel-commands/fuel-manager-interface.hpp
@@ -5,4 +5,5 @@ public:
     virtual int get_fuel_level() const = 0;
     virtual void set_fuel_level(int level) = 0;
     virtual int fuel_consumption() const  = 0;
+    virtual ~IFuelManager() = default;
 };
